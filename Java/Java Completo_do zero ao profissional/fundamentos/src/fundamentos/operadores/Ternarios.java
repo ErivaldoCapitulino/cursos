b1fd2c90ee075a9e55f3d79ddd45b2cb1e051810
Erivaldo Capitulino da Silva <vadositi@gmail.com>
2022-09-18 13:42:40 -0300

    Operadores Ternários

---
+++ Java/Java Completo_do zero ao profissional/fundamentos/src/fundamentos/operadores/Ternarios.java
@@ -0,0 +1,11 @@
+package fundamentos.operadores;
+
+public class Ternarios {
+
+	public static void main(String[] args) {
+		
+		double media = 7.6;
+		String resultadoFinal = media >= 7.0 ? "Aprovado" : "Reprovado";
+		System.out.println("O aluno está " + resultadoFinal);
+	}
+}
