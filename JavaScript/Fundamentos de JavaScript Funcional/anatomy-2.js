d6e47c290099488f5231e3c5b989bef5c197fa40
Erivaldo Capitulino da Silva <vadositi@gmail.com>
2022-09-18 23:38:57 -0300

    Function Express

---
+++ JavaScript/Fundamentos de JavaScript Funcional/anatomy-2.js
@@ -0,0 +1,15 @@
+//Anonymos function
+(function (a, b, c) {
+  return a + b + c;
+});
+
+/**
+ * Um função express é composta por uma função anonima
+ */
+//function express
+
+const sum = function (a, b) {
+  return a + b;
+};
+const result = sum(7, 59);
+console.log(result);
