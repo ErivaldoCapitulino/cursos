14da81822fdef13d15aebbb980c9acbed04fb464
Erivaldo Capitulino da Silva <vadositi@gmail.com>
2022-09-25 16:56:07 -0300

    Conversão String para número

--- Java/Java Completo_do zero ao profissional/fundamentos/src/fundamentos/ConversaoNumeroString.java
+++ Java/Java Completo_do zero ao profissional/fundamentos/src/fundamentos/ConversaoNumeroString.java
@@ -6,5 +6,11 @@ public class ConversaoNumeroString {
 		
 		Integer num1 = 10000;
 		System.out.println(num1.toString().length());
+		
+		int num2 = 10000;
+		System.out.println(Integer.toString(num2).length());
+		
+		System.out.println(("" +num1).length());
+		System.out.println(("" + num2).length());
 	}
 }
