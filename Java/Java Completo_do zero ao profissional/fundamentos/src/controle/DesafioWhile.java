e850fd572a58cc8e5644005cc519be1d09b506e4
Erivaldo Capitulino da Silva <vadositi@gmail.com>
2022-09-26 23:19:52 -0300

    For com resultado descedente

--- Java/Java Completo_do zero ao profissional/fundamentos/src/controle/DesafioWhile.java
+++ Java/Java Completo_do zero ao profissional/fundamentos/src/controle/DesafioWhile.java
@@ -7,14 +7,30 @@ public class DesafioWhile {
 	public static void main(String[] args) {
 
 		int i = 0;
+		int sair = 0;
 
 		Scanner entrada = new Scanner(System.in);
 
-		while (i == -1) {
+		int quantidadeDeNotas = 0;
+		double nota = 0;
+		double total = 0;
 
-			System.out.print("Deseja sair do sistema se sim digite -1 ");
-			i = entrada.nextInt();
+		while (nota != -1) {
+			System.out.println("Informe a nota: (ou -1 p/ sair): ");
+			nota = entrada.nextDouble();
+
+			if (nota <= 10 && nota >= 0) {
+
+				total += nota;
+				quantidadeDeNotas++;
+			}
+			else {
+				System.out.println("Nota inválida!!!");
+			}
 		}
+
+		double media = total / quantidadeDeNotas;
+		System.out.println("Média = " + media);
 
 		entrada.close();
 	}
