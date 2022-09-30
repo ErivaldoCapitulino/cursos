c94a59faeb582f2e5de9e44fab08ed58aedc43f4
Erivaldo Capitulino da Silva <vadositi@gmail.com>
2022-09-30 11:02:09 -0300

    Aula sobre construtor

--- Java/Java Completo_do zero ao profissional/exercicios/src/classes/Produto.java
+++ Java/Java Completo_do zero ao profissional/exercicios/src/classes/Produto.java
@@ -6,6 +6,20 @@ public class Produto {
 	double preco;
 	double desconto;
 	
+	Produto(){
+		
+	}
+
+	Produto(String nomeInicial){
+		nome = nomeInicial;
+	}
+	
+	Produto(String nomeInicial, double precoInicial, double descontoIncial){
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = descontoIncial;
+	}
+	
 	double precoComDesconto() {
 		return preco * (1 - desconto);
 	}
--- Java/Java Completo_do zero ao profissional/exercicios/src/classes/TesteProduto.java
+++ Java/Java Completo_do zero ao profissional/exercicios/src/classes/TesteProduto.java
@@ -4,22 +4,25 @@ public class TesteProduto {
 
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto();
-		p1.nome = "Notebook";
-		p1.preco = 3500.00;
-		p1.desconto = 0.2;
+		var p1 = new Produto();
+		p1.nome = "Bicicleta";
+		p1.preco = 650;
+		p1.desconto = 0.1;
 		
-		var p2 = new Produto();
-		p2.nome = "Bicicleta";
-		p2.preco = 650;
-		p2.desconto = 0.1;
+		Produto p2 = new Produto("Notebook");
+		p2.preco = 3500.00;
+		p2.desconto = 0.2;
+		
+		Produto p3 = new Produto("Tablet", 800, 0.1);
 		
 		System.out.println(p1.nome);
 		System.out.println(p2.nome);
+		System.out.println(p3.nome);
 		
 		double precoFinal1 = p1.precoComDesconto();
 		double precoFinal2 = p2.precoComDesconto();
-		double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
+		double precoFinal3 = p3.precoComDesconto();
+		double mediaCarrinho = (precoFinal1 + precoFinal2 + precoFinal3) / 3;
 		
 		System.out.printf("Média do carrinho = R$%.2f.", mediaCarrinho);
 	}
