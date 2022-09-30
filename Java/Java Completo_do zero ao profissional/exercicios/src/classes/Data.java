ca365fa88a629c3fa83f4a4fce196bc700eb9eef
Erivaldo Capitulino da Silva <vadositi@gmail.com>
2022-09-30 11:13:19 -0300

    Desafio construtor

--- Java/Java Completo_do zero ao profissional/exercicios/src/classes/Data.java
+++ Java/Java Completo_do zero ao profissional/exercicios/src/classes/Data.java
@@ -5,7 +5,20 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
-	
+
+	Data() {
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+
+	Data(int dia, int mes, int ano) {
+
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
 	String obterDataFormatada() {
 //		return dia + "/" + mes + "/" + ano;
 		return String.format("%d/%d/%d %n", dia, mes, ano);
--- Java/Java Completo_do zero ao profissional/exercicios/src/classes/DataTeste.java
+++ Java/Java Completo_do zero ao profissional/exercicios/src/classes/DataTeste.java
@@ -9,10 +9,7 @@ public class DataTeste {
 		d1.mes = 9;
 		d1.ano = 2022;
 		
-		Data d2 = new Data();
-		d2.dia = 1;
-		d2.mes = 9;
-		d2.ano = 2022;		
+		Data d2 = new Data(1,9,2022);
 		
 		System.out.println(d1.obterDataFormatada());
 		System.out.printf("%d/%d/%d",d2.dia, d2.mes, d2.ano);
